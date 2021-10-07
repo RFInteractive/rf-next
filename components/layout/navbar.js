37610c947e14a1ba4e14a5e30e71219f3a243977
Jamin <jaminwebdev@gmail.com>
2021-10-07 07:28:24 -0700

    Navbar component created with dummy content

--- components/layout/navbar.js
+++ components/layout/navbar.js
@@ -1,5 +1,25 @@
+import Link from 'next/link';
+
 const Navbar = () => {
-    return <h2>Navbar</h2>;
+    return (
+        <nav className="appNav">
+            <h1>Logo</h1>
+            <div className="appNav__menu">
+                <Link className="appNav__menuItem" href="/seo-services">
+                    <a>SEO</a>
+                </Link>
+                <Link className="appNav__menuItem" href="/seo-content">
+                    <a>Content</a>
+                </Link>
+                <Link
+                    className="appNav__menuItem"
+                    href="/web-design-and-development"
+                >
+                    <a>Design & Dev</a>
+                </Link>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
