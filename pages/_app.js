62956fa2d08ab44fc02193ed76c3d251ffea5ad4
Jamin <jaminwebdev@gmail.com>
2021-10-07 11:10:45 -0700

    theme-ui added +  base configuration

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
+/** @jsxImportSource theme-ui */
+import { ThemeProvider } from 'theme-ui';
+import { theme } from '../lib/theme';
+
 import '../styles/globals.css';
 import Layout from '../components/layout/layout';
 
 const MyApp = ({ Component, pageProps }) => {
     return (
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
+        <ThemeProvider theme={theme}>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </ThemeProvider>
     );
 };
 
