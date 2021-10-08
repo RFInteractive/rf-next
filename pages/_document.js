403e5a29f31a60de69cda412105b03b67192718f
Jamin <jaminwebdev@gmail.com>
2021-10-08 14:57:15 -0700

    font family import switched to Poppins

--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,7 @@ class MyDocument extends Document {
             <Html>
                 <Head>
                     <link
-                        href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;900&display=swap"
+                        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,900;1,400&display=swap"
                         rel="stylesheet"
                     />
                 </Head>
