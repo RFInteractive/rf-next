3f7e5df37dbf63b84dc2c046e38aed351e7d7b5b
Jamin <jaminwebdev@gmail.com>
2021-10-11 19:25:31 -0700

    scss support added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
+const path = require('path');
+
 module.exports = {
     reactStrictMode: true,
     experimental: {
         scrollRestoration: true,
+    },
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
     },
 };
