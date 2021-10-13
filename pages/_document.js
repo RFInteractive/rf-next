88f8feefafe9047b3cd6091e54538655b0aa0658
Jamin <jaminwebdev@gmail.com>
2021-10-13 12:34:21 -0700

    favicon moved to _document

--- pages/_document.js
+++ pages/_document.js
@@ -15,6 +15,7 @@ class MyDocument extends Document {
                         href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,900;1,400&display=swap"
                         rel="stylesheet"
                     />
+                    <link rel="icon" href="/favicon.ico" />
                 </Head>
                 <body>
                     <Main />
