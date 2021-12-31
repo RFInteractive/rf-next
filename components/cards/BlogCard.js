24c925b50b29002a22632482ca8d0f6ee645c06f
Jamin <jaminwebdev@gmail.com>
2021-12-30 16:33:48 -0800

    basic blog page, fetching, and pagination implemented

---
+++ components/cards/BlogCard.js
@@ -0,0 +1,19 @@
+/* eslint-disable @next/next/no-img-element */
+/** @jsxImportSource theme-ui */
+import Link from 'next/link';
+
+const BlogCard = ({ post }) => {
+    return (
+        <div>
+            <h3>{post.title}</h3>
+            <h5>{post.link}</h5>
+            <img src={post.featuredImg} alt="" />
+            <p>{post.date}</p>
+            <p>{post.author}</p>
+            <div dangerouslySetInnerHTML={{ __html: post.excerpt }}></div>
+            <p>{post.categories}</p>
+        </div>
+    );
+};
+
+export default BlogCard;
