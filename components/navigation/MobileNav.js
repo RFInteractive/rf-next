2ec6b82a4abc16be49a5ffb34cf4f89eee1a6665
Jamin <jaminwebdev@gmail.com>
2022-01-07 17:58:58 -0800

    logic for show/hide of mobile portall added to mobile nav component

--- components/navigation/MobileNav.js
+++ components/navigation/MobileNav.js
@@ -1,9 +1,23 @@
 /** @jsxImportSource theme-ui */
+import { useState } from 'react';
+
+import MobileMenuPortal from '../portals/mobileMenuPortal';
 
 const MobileNav = () => {
+    const [open, setOpen] = useState(false);
+
     return (
-        <nav sx={{ display: ['block', 'block', 'none', 'none'] }}>
+        <nav
+            sx={{
+                display: ['block', 'block', 'none', 'none'],
+                position: 'fixed',
+            }}
+        >
             <h1>Mobile Nav</h1>
+            <button onClick={() => setOpen(true)}>open</button>
+            <MobileMenuPortal open={open} onClose={() => setOpen(false)}>
+                Mobile Portal
+            </MobileMenuPortal>
         </nav>
     );
 };
