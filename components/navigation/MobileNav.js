824de736af7d287fe4839f93d8d75efd0517df3e
Jamin <jaminwebdev@gmail.com>
2022-01-13 12:36:00 -0800

    mobile menu placed in own component within mobile nav component

--- components/navigation/MobileNav.js
+++ components/navigation/MobileNav.js
@@ -12,45 +12,51 @@ const MobileNav = () => {
 
     return (
         <AnimatePresence exitBeforeEnter>
-            {open ? (
-                <MobileMenuModal setOpen={setOpen} />
+            {!open ? (
+                <MobileMenu setOpen={setOpen} key="mobileMenu" />
             ) : (
-                <motion.div
-                    key="mobileMenu"
-                    initial={{ opacity: 0, y: '-500px' }}
-                    animate={{
-                        opacity: 1,
-                        y: '0px',
-                        position: 'fixed',
-                        top: '20px',
-                        width: '100vw',
-                        zIndex: '100000',
-                        display: 'flex',
-                        justifyContent: 'center',
-                        transition: {
-                            duration: 0.2,
-                        },
-                    }}
-                    exit={{ opacity: 0, y: '-100px' }}
-                >
-                    <nav
-                        sx={{
-                            display: ['flex', 'flex', 'none', 'none'],
-                            justifyContent: 'space-between',
-                            position: 'fixed',
-                            width: '90%',
-                            backgroundColor: '#fff',
-                            padding: '10px 15px',
-                            borderRadius: '10px',
-                            boxShadow: '0px 0px 15px rgba(0, 0, 0, 0.1)',
-                        }}
-                    >
-                        <h1>Mobile Nav</h1>
-                        <button onClick={() => setOpen(true)}>open</button>
-                    </nav>
-                </motion.div>
+                <MobileMenuModal setOpen={setOpen} key="mobileMenuModal" />
             )}
         </AnimatePresence>
+    );
+};
+
+const MobileMenu = ({ setOpen }) => {
+    return (
+        <motion.div
+            key="mobileMenu"
+            initial={{ opacity: 0, y: '-500px' }}
+            animate={{
+                opacity: 1,
+                y: '0px',
+                position: 'fixed',
+                top: '20px',
+                width: '100vw',
+                zIndex: '100000',
+                display: 'flex',
+                justifyContent: 'center',
+                transition: {
+                    duration: 0.2,
+                },
+            }}
+            exit={{ opacity: 0, y: '-100px' }}
+        >
+            <nav
+                sx={{
+                    display: ['flex', 'flex', 'none', 'none'],
+                    justifyContent: 'space-between',
+                    position: 'fixed',
+                    width: '90%',
+                    backgroundColor: '#fff',
+                    padding: '10px 15px',
+                    borderRadius: '10px',
+                    boxShadow: '0px 0px 15px rgba(0, 0, 0, 0.1)',
+                }}
+            >
+                <h1>Mobile Nav</h1>
+                <button onClick={() => setOpen(true)}>open</button>
+            </nav>
+        </motion.div>
     );
 };
 
