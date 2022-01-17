8ae04b8bd46ad34c1fedbeb7eda016ac05af3471
Jamin <jaminwebdev@gmail.com>
2022-01-17 11:54:17 -0800

    anchor href bug fixed on blog cards

--- components/cards/BlogCard.js
+++ components/cards/BlogCard.js
@@ -6,66 +6,75 @@ import { Flex } from 'theme-ui';
 const BlogCard = ({ post }) => {
     return (
         <Link href={post.uri} passHref>
-            <div
-                sx={{
-                    padding: '25px',
-                    borderRadius: '20px',
-                    boxShadow: '0px 0px 20px rgba(0, 0, 0, 0.12)',
-                    backgroundColor: 'muted',
-                    marginTop: '50px',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'space-between',
-                    '&:hover': {
-                        cursor: 'pointer',
-                        transform: 'scale(1.02)',
-                        transition: 'all 0.2s ease-out',
-                    },
-                }}
-            >
-                <img
-                    src={post.featuredImg}
-                    alt={`Featured image for post titled "${post.title}"`}
+            <a>
+                <div
                     sx={{
-                        width: '100%',
-                        height: '245px',
-                        objectFit: 'cover',
-                        marginTop: '-65px',
-                        borderRadius: '10px',
+                        padding: '25px',
+                        borderRadius: '20px',
                         boxShadow: '0px 0px 20px rgba(0, 0, 0, 0.12)',
-                        marginBottom: '20px',
-                    }}
-                />
-
-                <h3
-                    sx={{
-                        fontSize: '24px',
-                        marginBottom: '20px',
+                        backgroundColor: 'muted',
+                        marginTop: '50px',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'space-between',
+                        '&:hover': {
+                            cursor: 'pointer',
+                            transform: 'scale(1.02)',
+                            transition: 'all 0.2s ease-out',
+                        },
                     }}
                 >
-                    {post.title}
-                </h3>
+                    <img
+                        src={post.featuredImg}
+                        alt={`Featured image for post titled "${post.title}"`}
+                        sx={{
+                            width: '100%',
+                            height: '245px',
+                            objectFit: 'cover',
+                            marginTop: '-65px',
+                            borderRadius: '10px',
+                            boxShadow: '0px 0px 20px rgba(0, 0, 0, 0.12)',
+                            marginBottom: '20px',
+                        }}
+                    />
 
-                <div
-                    dangerouslySetInnerHTML={{ __html: post.excerpt }}
-                    sx={{ mb: '30px', fontSize: '18px' }}
-                ></div>
+                    <h3
+                        sx={{
+                            fontSize: '24px',
+                            marginBottom: '20px',
+                            color: 'text',
+                        }}
+                    >
+                        {post.title}
+                    </h3>
 
-                <hr sx={{ my: '10px', borderTop: '#a9a9a9' }}></hr>
+                    <div
+                        dangerouslySetInnerHTML={{ __html: post.excerpt }}
+                        sx={{ mb: '30px', fontSize: '18px', color: 'text' }}
+                    ></div>
 
-                <Flex sx={{ justifyContent: 'space-between' }}>
-                    <div>
-                        <p sx={{ fontSize: '14px', color: '#a9a9a9' }}>
-                            {post.author}
-                        </p>
-                    </div>
-                    <div>
-                        <p sx={{ fontSize: '14px', color: '#a9a9a9' }}>
-                            {post.date}
-                        </p>
-                    </div>
-                </Flex>
-            </div>
+                    <hr
+                        sx={{
+                            my: '10px',
+                            borderTop: '#a9a9a9',
+                            color: '#a9a9a9',
+                        }}
+                    ></hr>
+
+                    <Flex sx={{ justifyContent: 'space-between' }}>
+                        <div>
+                            <p sx={{ fontSize: '14px', color: '#a9a9a9' }}>
+                                {post.author}
+                            </p>
+                        </div>
+                        <div>
+                            <p sx={{ fontSize: '14px', color: '#a9a9a9' }}>
+                                {post.date}
+                            </p>
+                        </div>
+                    </Flex>
+                </div>
+            </a>
         </Link>
     );
 };
