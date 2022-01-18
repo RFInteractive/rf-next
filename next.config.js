adec0aa47d3f0c6676323ea814fd3e8cc448b0bf
Jamin <jaminwebdev@gmail.com>
2022-01-18 08:07:50 -0800

    experimental scroll restoration removed

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ const path = require('path');
 
 module.exports = {
     reactStrictMode: true,
-    experimental: {
-        scrollRestoration: true,
-    },
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
     },
