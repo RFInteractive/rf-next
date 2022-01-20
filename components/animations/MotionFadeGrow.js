f889778a726e5f2c882320984e811389b152ee5f
Jamin <jaminwebdev@gmail.com>
2022-01-19 19:22:56 -0800

    motion fade wrapper component timing adjusted

--- components/animations/MotionFadeGrow.js
+++ components/animations/MotionFadeGrow.js
@@ -1,49 +1,30 @@
 import { motion, useAnimation } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
 
-const MotionFadeGrow = ({ delay = 0.4, lazy = false, children }) => {
+const MotionFadeGrow = ({ delay = 0.3, duration = 0.6, children }) => {
     const { inView, entry, ref } = useInView();
     const animationControl = useAnimation();
 
     if (inView) {
         animationControl.start({
-            scale: 1,
+            scale: [1, 1.03, 1],
             opacity: 1,
             transition: {
                 delay,
+                duration,
             },
         });
     }
 
-    if (lazy) {
-        return (
-            <div ref={ref}>
-                <motion.div
-                    initial={{ scale: 0.8, opacity: 0 }}
-                    animate={animationControl}
-                >
-                    {children}
-                </motion.div>
-            </div>
-        );
-    }
-
     return (
-        <motion.div
-            initial={{
-                scale: 0.8,
-                opacity: 0,
-            }}
-            animate={{
-                scale: [1, 1.03, 1],
-                opacity: 1,
-                transition: {
-                    delay,
-                },
-            }}
-        >
-            {children}
-        </motion.div>
+        <div ref={ref}>
+            <motion.div
+                initial={{ scale: 0.8, opacity: 0 }}
+                animate={animationControl}
+            >
+                {children}
+            </motion.div>
+        </div>
     );
 };
 
