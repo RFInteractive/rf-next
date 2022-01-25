353924a95503b96f107cbe16ef7c1ebdb0bd2793
Jamin <jaminwebdev@gmail.com>
2022-01-24 17:19:29 -0800

    Revert "template driven initial framer motion state replaced with imperative control"
    This reverts commit dc1c6e8a29d725f83fb8cd11febe8403419dfacc.

--- components/animations/MotionFadeGrow.js
+++ components/animations/MotionFadeGrow.js
@@ -1,25 +1,14 @@
 import { motion, useAnimation } from 'framer-motion';
-import { useEffect } from 'react';
 import { useInView } from 'react-intersection-observer';
 
 const MotionFadeGrow = ({ delay = 0.3, duration = 0.6, children }) => {
-    const { inView, entry, ref } = useInView({
-        triggerOnce: true,
-        threshold: 0.3,
-    });
+    const { inView, entry, ref } = useInView({ triggerOnce: true });
     const animationControl = useAnimation();
-
-    useEffect(() => {
-        animationControl.start({
-            scale: 0.8,
-            opacity: 0,
-        });
-    }, [animationControl]);
 
     if (inView) {
         animationControl.start({
             scale: [1, 1.03, 1],
-            opacity: [0, 1],
+            opacity: 1,
             transition: {
                 delay,
                 duration,
@@ -29,7 +18,12 @@ const MotionFadeGrow = ({ delay = 0.3, duration = 0.6, children }) => {
 
     return (
         <div ref={ref}>
-            <motion.div animate={animationControl}>{children}</motion.div>
+            <motion.div
+                initial={{ scale: 0.8, opacity: 0 }}
+                animate={animationControl}
+            >
+                {children}
+            </motion.div>
         </div>
     );
 };
