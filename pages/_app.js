7ed43f01b0cd28d6fa383491b4ea5baa3002d349
Jamin <jaminwebdev@gmail.com>
2022-01-26 09:39:49 -0800

    delay removed from _app route transition

--- pages/_app.js
+++ pages/_app.js
@@ -22,7 +22,6 @@ const MyApp = ({ Component, pageProps, router }) => {
                             opacity: 1,
                             transition: {
                                 duration: 0.6,
-                                delay: 0.6,
                             },
                         }}
                         exit={{
