3c414425956b5dae41d97ecc2577eb22847cbc76
Jamin <jaminwebdev@gmail.com>
2022-02-21 18:40:24 -0800

    redirect helper imported into next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 const path = require('path');
+const myRedirects = require('./lib/redirects');
 
 module.exports = {
     reactStrictMode: true,
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
+    },
+    async redirects() {
+        return [...myRedirects.customRedirects];
     },
 };
