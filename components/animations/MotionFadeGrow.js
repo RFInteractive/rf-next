6d502531878efbbc87bc0ddd61998f8b93f78127
Jamin <jaminwebdev@gmail.com>
2022-02-24 18:44:05 -0800

    MotionFadeGrow threshold and delay defaults enforced for mobile

--- components/animations/MotionFadeGrow.js
+++ components/animations/MotionFadeGrow.js
@@ -1,5 +1,6 @@
 import { motion, useAnimation, AnimatePresence } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
+import useWindowDimensions from '../../hooks/useWindowDimensions';
 
 const MotionFadeGrow = ({
     delay = 0,
@@ -7,15 +8,19 @@ const MotionFadeGrow = ({
     threshold = 0,
     children,
 }) => {
-    const { inView, entry, ref } = useInView({ triggerOnce: true, threshold });
     const animationControl = useAnimation();
+    const { windowWidth } = useWindowDimensions();
+    const { inView, entry, ref } = useInView({
+        triggerOnce: true,
+        threshold: windowWidth > 800 ? threshold : 0.8,
+    });
 
     if (inView) {
         animationControl.start({
             scale: [1, 1.03, 1],
             opacity: 1,
             transition: {
-                delay,
+                delay: windowWidth > 800 ? delay : 0,
                 duration,
             },
         });
