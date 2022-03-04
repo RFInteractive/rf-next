56cf4a3cbc596af9d68633bbd52e15d009feb9dd
Jamin <jaminwebdev@gmail.com>
2022-03-03 19:04:39 -0800

    replace :contains with .contains() call

--- cypress/integration/desktop-nav.spec.js
+++ cypress/integration/desktop-nav.spec.js
@@ -7,16 +7,17 @@ describe('Basic homepage behaviors', () => {
     });
 
     it('should navigate to SEO Services page', () => {
-        cy.get(
-            '.desktopNavLinkContainer .desktopNavLink:contains("SEO")'
-        ).click();
+        cy.get('.desktopNavLinkContainer .desktopNavLink')
+            .contains('SEO')
+            .click();
 
         cy.location('pathname').should('include', 'seo-services');
         cy.get('h1').should('include.text', 'Traffic You Deserve');
     });
 
     it('should navigate to off-page SEO on SEO Services Page', () => {
-        cy.get('.desktopNavLinkContainer .desktopNavLink:contains("SEO")')
+        cy.get('.desktopNavLinkContainer .desktopNavLink')
+            .contains('SEO')
             .next()
             .find('a:contains("Local SEO")')
             .click({ force: true });
--- cypress/integration/general-multi-step-form.spec.js
+++ cypress/integration/general-multi-step-form.spec.js
@@ -1,55 +1,68 @@
 /// <reference types="cypress" />
 
 describe('GeneralMultiStepForm rendering, behavior, and validation', () => {
-    it('should render the form when triggered', () => {
-        cy.visit('http://localhost:3000/');
-        cy.get(`header button:contains("Let's Work Together")`).click();
-        cy.wait(600);
-        cy.get('[data-cy=GeneralMultiStepForm]').should('be.visible');
+    describe('Form rendering on homepage header button click', () => {
+        it('should render the form when triggered', () => {
+            cy.visit('http://localhost:3000/');
+            cy.get('header button').contains("Let's Work Together").click();
+            cy.wait(600);
+            cy.get('[data-cy=GeneralMultiStepForm]').should('be.visible');
+        });
     });
 
-    it('should check the SEO and Content checkboxes and Next should be visible', () => {
-        cy.get('[data-cy=GeneralMultiStepForm] input[value="SEO"]').click({
-            force: true,
-        });
-        cy.get('[data-cy=GeneralMultiStepForm] input[value="SEO"]').should(
-            'be.checked'
-        );
+    describe('Checking steps and validation', () => {
+        it('should check the SEO and Content checkboxes and Next should be visible', () => {
+            cy.visit('http://localhost:3000/');
+            cy.get('header button').contains("Let's Work Together").click();
 
-        cy.get('[data-cy=GeneralMultiStepForm] input[value="Content"]').click({
-            force: true,
-        });
-        cy.get('[data-cy=GeneralMultiStepForm] input[value="Content"]').should(
-            'be.checked'
-        );
+            cy.get('[data-cy=GeneralMultiStepForm] input[value="SEO"]').click({
+                force: true,
+            });
+            cy.get('[data-cy=GeneralMultiStepForm] input[value="SEO"]').should(
+                'be.checked'
+            );
 
-        cy.get('[data-cy=GeneralMultiStepFirstStepNextBtn]').should(
-            'be.visible'
-        );
-    });
+            cy.get(
+                '[data-cy=GeneralMultiStepForm] input[value="Content"]'
+            ).click({
+                force: true,
+            });
+            cy.get(
+                '[data-cy=GeneralMultiStepForm] input[value="Content"]'
+            ).should('be.checked');
 
-    it('next step button for step 1 should be visible, then invisible', () => {
-        cy.get('[data-cy=GeneralMultiStepForm] input[value="SEO"]').click({
-            force: true,
-        });
-        cy.get('[data-cy=GeneralMultiStepForm] input[value="Content"]').click({
-            force: true,
+            cy.get('[data-cy=GeneralMultiStepFirstStepNextBtn]').should(
+                'be.visible'
+            );
         });
 
-        cy.get('[data-cy=GeneralMultiStepFirstStepNextBtn]').should(
-            'not.exist'
-        );
-    });
+        it('next step button for step 1 should be visible, then invisible', () => {
+            cy.get('[data-cy=GeneralMultiStepForm] input[value="SEO"]').click({
+                force: true,
+            });
+            cy.get(
+                '[data-cy=GeneralMultiStepForm] input[value="Content"]'
+            ).click({
+                force: true,
+            });
 
-    it('should move on to step 2 after checking SEO', () => {
-        cy.get('[data-cy=GeneralMultiStepForm] input[value="Content"]').click({
-            force: true,
+            cy.get('[data-cy=GeneralMultiStepFirstStepNextBtn]').should(
+                'not.exist'
+            );
         });
 
-        cy.get('[data-cy=GeneralMultiStepFirstStepNextBtn]').click();
+        it('should move on to step 2 after checking SEO', () => {
+            cy.get(
+                '[data-cy=GeneralMultiStepForm] input[value="Content"]'
+            ).click({
+                force: true,
+            });
 
-        cy.get('p:contains("Do you have a website already? *")').should(
-            'be.visible'
-        );
+            cy.get('[data-cy=GeneralMultiStepFirstStepNextBtn]').click();
+
+            cy.get('p:contains("Do you have a website already? *")').should(
+                'be.visible'
+            );
+        });
     });
 });
